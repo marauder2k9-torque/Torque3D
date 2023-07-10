10e8174b2acbf1d291580421e241157caa9a1ba9
marauder2k7 <ctrl-intelligence@live.co.uk>
2023-07-10 16:46:21 +0100

    Update cTestC.hlsl

--- Templates/BaseGame/game/core/rendering/shaders/ComputeExample/cTestC.hlsl
+++ Templates/BaseGame/game/core/rendering/shaders/ComputeExample/cTestC.hlsl
@@ -1,10 +1,10 @@
 
-RWTexture2D<unorm float4> BufferOut;
+RWTexture2D<float4> BufferOut;
 
-[numthreads(8, 8, 1)]
+[numthreads(32, 32, 1)]
 void main(uint3 id : SV_DispatchThreadID)
 {
-    float4 col = float4(0.0, 0.0, 0.0, 1.0);
+    float4 col = float4(1.0, 0.0, 0.0, 1.0);
 
     
     BufferOut[id.xy] = float4(id.x & id.y, (id.x & 15)/15.0, (id.y & 15)/15.0, 0.0);
