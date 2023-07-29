27d966fdf05d3cbc1ce2e8a5ede8f15ac0ca576b
marauder2k7 <ctrl-intelligence@live.co.uk>
2023-07-29 10:39:35 +0100

    Update torque_ios_config.cmake

--- Tools/CMake/torque_ios_config.cmake
+++ Tools/CMake/torque_ios_config.cmake
@@ -99,6 +99,42 @@ if(IOS)
     
     set (CMAKE_SYSTEM_NAME iOS)
 
+	# Force the compilers to gcc for iOS
+	include (CMakeForceCompiler)
+	CMAKE_FORCE_C_COMPILER (gcc gcc)
+	CMAKE_FORCE_CXX_COMPILER (g++ g++)
+
+	# Skip the platform compiler checks for cross compiling
+	set (CMAKE_CXX_COMPILER_WORKS TRUE)
+	set (CMAKE_C_COMPILER_WORKS TRUE)
+
+	# All iOS/Darwin specific settings - some may be redundant
+	set (CMAKE_SHARED_LIBRARY_PREFIX "lib")
+	set (CMAKE_SHARED_LIBRARY_SUFFIX ".dylib")
+	set (CMAKE_SHARED_MODULE_PREFIX "lib")
+	set (CMAKE_SHARED_MODULE_SUFFIX ".so")
+	set (CMAKE_MODULE_EXISTS 1)
+	set (CMAKE_DL_LIBS "")
+
+	set (CMAKE_C_OSX_COMPATIBILITY_VERSION_FLAG "-compatibility_version ")
+	set (CMAKE_C_OSX_CURRENT_VERSION_FLAG "-current_version ")
+	set (CMAKE_CXX_OSX_COMPATIBILITY_VERSION_FLAG "${CMAKE_C_OSX_COMPATIBILITY_VERSION_FLAG}")
+	set (CMAKE_CXX_OSX_CURRENT_VERSION_FLAG "${CMAKE_C_OSX_CURRENT_VERSION_FLAG}")
+
+	# Hidden visibilty is required for cxx on iOS 
+	set (CMAKE_C_FLAGS "")
+	set (CMAKE_CXX_FLAGS "-headerpad_max_install_names -fvisibility=hidden -fvisibility-inlines-hidden")
+
+	set (CMAKE_C_LINK_FLAGS "-Wl,-search_paths_first ${CMAKE_C_LINK_FLAGS}")
+	set (CMAKE_CXX_LINK_FLAGS "-Wl,-search_paths_first ${CMAKE_CXX_LINK_FLAGS}")
+
+	set (CMAKE_PLATFORM_HAS_INSTALLNAME 1)
+	set (CMAKE_SHARED_LIBRARY_CREATE_C_FLAGS "-dynamiclib -headerpad_max_install_names")
+	set (CMAKE_SHARED_MODULE_CREATE_C_FLAGS "-bundle -headerpad_max_install_names")
+	set (CMAKE_SHARED_MODULE_LOADER_C_FLAG "-Wl,-bundle_loader,")
+	set (CMAKE_SHARED_MODULE_LOADER_CXX_FLAG "-Wl,-bundle_loader,")
+	set (CMAKE_FIND_LIBRARY_SUFFIXES ".dylib" ".so" ".a")
+
 	# hardcode CMAKE_INSTALL_NAME_TOOL here to install_name_tool, so it behaves as it did before, Alex
 	if (NOT DEFINED CMAKE_INSTALL_NAME_TOOL)
 		find_program(CMAKE_INSTALL_NAME_TOOL install_name_tool)
