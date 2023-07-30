2ff346ff8aa65aeddd4408f072c7b9bb17d40ca9
marauder2k7 <ctrl-intelligence@live.co.uk>
2023-07-30 08:35:29 +0100

    ios configs
    -IOS framework location

--- Tools/CMake/torque_macros.cmake
+++ Tools/CMake/torque_macros.cmake
@@ -76,6 +76,12 @@ macro (advanced_option flag description state)
     option(${flag} ${description} ${state})
     mark_as_advanced(${flag})
 endmacro(advanced_option)
+
+macro (advanced_set cmakeVar valVar docs)
+    set(${cmakeVar} ${valVar} CACHE STRING ${docs})
+    mark_as_advanced(${cmakeVar})
+endmacro(advanced_set)
+
 ################# additional preprocessor defines ###################
 macro(__addDef def config)
     # two possibilities: a) target already known, so add it directly, or b) target not yet known, so add it to its cache
