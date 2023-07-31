c610c33a0532069b4a0ecf679f5d2b6ccb1dda73
marauder2k7 <ctrl-intelligence@live.co.uk>
2023-07-31 11:07:52 +0100

    Revert "cmake restructure"
    This reverts commit d4b98943856474b482fcb8d5cba3b480c572f719.

--- Tools/CMake/torque_configs.cmake
+++ Tools/CMake/torque_configs.cmake
@@ -1,7 +1,24 @@
-# An application name must be set first
-set(TORQUE_APP_NAME "" CACHE STRING "the app name")
-if("${TORQUE_APP_NAME}" STREQUAL "")
-	message(FATAL_ERROR "Please set TORQUE_APP_NAME first")
+################# Initialize Common Variables ###################
+
+# All include directories to search. Modules should append to this when they want includes to point
+# into themselves.
+set(TORQUE_INCLUDE_DIRECTORIES "")
+
+# All library binaries to install. Modules should append to this the path of any library binaries (.so, .dylib, .dll)
+# that should be installed next to the executable.
+set(TORQUE_ADDITIONAL_LIBRARY_BINARIES "")
+
+# All compile definitions. Modules should append to this if there is any special defines needed.
+set(TORQUE_COMPILE_DEFINITIONS ICE_NO_DLL PCRE_STATIC TORQUE_ADVANCED_LIGHTING TORQUE_SHADERGEN
+							   TORQUE_OPCODE TORQUE_ASSIMP TORQUE_SDL TORQUE_COLLADA
+							   TORQUE_UNICODE UNICODE _UNICODE)
+
+# All link libraries. Modules should append to this the path to specify additional link libraries (.a, .lib, .dylib, .so)
+set(TORQUE_LINK_LIBRARIES tinyxml collada ljpeg squish png_static opcode assimp
+                          SDL2 glad pcre convexDecomp zlib)
+
+if(TORQUE_TESTING)
+set(TORQUE_LINK_LIBRARIES ${TORQUE_LINK_LIBRARIES} gtest gmock)
 endif()
 
 if(NOT WIN32)
