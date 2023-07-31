8d4fab96dca988317d8889625b7663bc1b2fb68f
marauder2k7 <ctrl-intelligence@live.co.uk>
2023-07-31 13:58:12 +0100

    advanced set

--- Tools/CMake/iOS.cmake
+++ Tools/CMake/iOS.cmake
@@ -77,49 +77,49 @@
 set(TORQUE_LINK_LIBRARIES tinyxml collada ljpeg squish png_static opcode glad pcre zlib)
 
 #general
-advanced_option(TORQUE_MULTITHREAD "Multi Threading" ON)
+set(TORQUE_MULTITHREAD ON CACHE BOOL "" FORCE)
 advanced_option(TORQUE_DISABLE_MEMORY_MANAGER "Disable memory manager" ON)
 
 #fileIO
 set(TORQUE_APP_PASSWORD "changeme" CACHE STRING "zip file password")
-advanced_option(TORQUE_DISABLE_VIRTUAL_MOUNT_SYSTEM "Disable virtual mount system" OFF)
-advanced_option(TORQUE_DISABLE_FIND_ROOT_WITHIN_ZIP "Disable reading root path from zip. Zips will be mounted in-place with file name as directory name." ON)
-advanced_option(TORQUE_ZIP_DISK_LAYOUT "All zips must be placed in the executable directory and contain full paths to the files." OFF)
-advanced_option(TORQUE_POSIX_PATH_CASE_INSENSITIVE "POSIX Pathing Case Insensitivity" ON)
-advanced_option(TORQUE_ZIP_PATH_CASE_INSENSITIVE "ZIP Pathing Case Insensitivity" ON)
-advanced_option(TORQUE_USE_ZENITY "use the Zenity backend for NFD" OFF)
-advanced_option(TORQUE_SECURE_VFS "Secure VFS configuration. Arbitrary script access to file system will be heavily restricted." OFF)
+advanced_set(TORQUE_DISABLE_VIRTUAL_MOUNT_SYSTEM "Disable virtual mount system" OFF)
+advanced_set(TORQUE_DISABLE_FIND_ROOT_WITHIN_ZIP "Disable reading root path from zip. Zips will be mounted in-place with file name as directory name." ON)
+advanced_set(TORQUE_ZIP_DISK_LAYOUT "All zips must be placed in the executable directory and contain full paths to the files." OFF)
+advanced_set(TORQUE_POSIX_PATH_CASE_INSENSITIVE "POSIX Pathing Case Insensitivity" ON)
+advanced_set(TORQUE_ZIP_PATH_CASE_INSENSITIVE "ZIP Pathing Case Insensitivity" ON)
+advanced_set(TORQUE_USE_ZENITY "use the Zenity backend for NFD" OFF)
+advanced_set(TORQUE_SECURE_VFS "Secure VFS configuration. Arbitrary script access to file system will be heavily restricted." OFF)
 
 #sfx
-advanced_option(TORQUE_SFX_VORBIS "Vorbis Sound" OFF)
-advanced_option(TORQUE_THEORA "Theora Video Support" OFF)
-advanced_option(TORQUE_SFX_OPENAL "OpenAL Sound" OFF)
+advanced_set(TORQUE_SFX_VORBIS "Vorbis Sound" OFF)
+advanced_set(TORQUE_THEORA "Theora Video Support" OFF)
+advanced_set(TORQUE_SFX_OPENAL "OpenAL Sound" OFF)
 
 #gfx
-advanced_option(TORQUE_DEBUG_GFX_MODE "triggers graphics debug mode" OFF)
-advanced_option(TORQUE_ADVANCED_LIGHTING "Advanced Lighting" ON)
-advanced_option(TORQUE_BASIC_LIGHTING "Basic Lighting" ON)
-advanced_option(TORQUE_OPENGL "Allow OpenGL render" ON) # we need OpenGL to render on Linux/Mac
+advanced_set(TORQUE_DEBUG_GFX_MODE "triggers graphics debug mode" OFF)
+advanced_set(TORQUE_ADVANCED_LIGHTING "Advanced Lighting" ON)
+advanced_set(TORQUE_BASIC_LIGHTING "Basic Lighting" ON)
+advanced_set(TORQUE_OPENGL "Allow OpenGL render" ON) # we need OpenGL to render on Linux/Mac
 
 #mode
-advanced_option(TORQUE_NO_DSO_GENERATION "skip storing compiled scripts" ON)
-advanced_option(TORQUE_DYNAMIC_LIBRARY "Whether or not to build Torque as a dynamic library." OFF)
-advanced_option(TORQUE_PLAYER "Playback only?" OFF)
-advanced_option(TORQUE_DEBUG "T3D Debug mode" OFF)
+advanced_set(TORQUE_NO_DSO_GENERATION "skip storing compiled scripts" ON)
+advanced_set(TORQUE_DYNAMIC_LIBRARY "Whether or not to build Torque as a dynamic library." OFF)
+advanced_set(TORQUE_PLAYER "Playback only?" OFF)
+advanced_set(TORQUE_DEBUG "T3D Debug mode" OFF)
 #option(DEBUG_SPEW "more debug" OFF)
-advanced_option(TORQUE_SHIPPING "T3D Shipping build?" OFF)
-advanced_option(TORQUE_DEDICATED "Torque dedicated" OFF) # disables compiling in gfx and sfx frontend functionality
+advanced_set(TORQUE_SHIPPING "T3D Shipping build?" OFF)
+advanced_set(TORQUE_DEDICATED "Torque dedicated" OFF) # disables compiling in gfx and sfx frontend functionality
 
 #tools
-advanced_option(TORQUE_DEBUG_NET "debug network" OFF)
-advanced_option(TORQUE_DEBUG_NET_MOVES "debug network moves" OFF)
-advanced_option(TORQUE_ENABLE_ASSERTS "enables or disable asserts" OFF)
-advanced_option(TORQUE_TOOLS "Enable or disable the tools" OFF)
-advanced_option(TORQUE_ENABLE_PROFILER "Enable or disable the profiler" OFF)
-advanced_option(TORQUE_SHOW_LEGACY_FILE_FIELDS "If on, shows legacy direct file path fields in the inspector." OFF)
+advanced_set(TORQUE_DEBUG_NET "debug network" OFF)
+advanced_set(TORQUE_DEBUG_NET_MOVES "debug network moves" OFF)
+advanced_set(TORQUE_ENABLE_ASSERTS "enables or disable asserts" OFF)
+advanced_set(TORQUE_TOOLS "Enable or disable the tools" OFF)
+advanced_set(TORQUE_ENABLE_PROFILER "Enable or disable the profiler" OFF)
+advanced_set(TORQUE_SHOW_LEGACY_FILE_FIELDS "If on, shows legacy direct file path fields in the inspector." OFF)
 
 #testing
-advanced_option(TORQUE_TESTING "Unit test build" OFF)
+advanced_set(TORQUE_TESTING "Unit test build" OFF)
 
 execute_process(COMMAND xcodebuild -version
   OUTPUT_VARIABLE XCODE_VERSION
