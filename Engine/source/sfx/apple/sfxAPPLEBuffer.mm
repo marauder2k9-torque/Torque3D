176d30a5036e1026fdbd7b9153febc5e7006e55d
marauder2k7 <ctrl-intelligence@live.co.uk>
2024-08-05 11:13:10 +0100

    Update sfxAPPLEBuffer.mm
    fill out the buffer

--- Engine/source/sfx/apple/sfxAPPLEBuffer.mm
+++ Engine/source/sfx/apple/sfxAPPLEBuffer.mm
@@ -71,7 +71,61 @@ SFXAPPLEBuffer::SFXAPPLEBuffer(const ThreadSafeRef<SFXStream> &stream, SFXDescri
 
 
 void SFXAPPLEBuffer::write(SFXInternal::SFXStreamPacket *const *packets, U32 num) {
+	if (!num || !mAVBuffer)
+	{
+		return;
+	}
 
+	// If this is not a streaming buffer, load the data into our single static buffer.
+	if (!isStreaming())
+	{
+		SFXInternal::SFXStreamPacket* packet = packets[num - 1];
+
+		// Ensure format is valid and create AVAudioPCMBuffer.
+		AVAudioFormat* format = mAVBuffer->format;
+		if (!format) 
+		{
+			return;
+		}
+
+		if (mAVBuffer->pcmBuffer) 
+		{
+			[mAVBuffer->pcmBuffer release];
+		}
+		
+		mAVBuffer->pcmBuffer = [[AVAudioPCMBuffer alloc] initWithPCMFormat:format frameCapacity:packet->mSizeActual / format.streamDescription->mBytesPerFrame];
+		mAVBuffer->pcmBuffer.frameLength = mAVBuffer->pcmBuffer.frameCapacity;
+
+		// Copy data to AVAudioPCMBuffer
+		memcpy(mAVBuffer->pcmBuffer.audioBufferList->mBuffers[0].mData, packet->data, packet->mSizeActual);
+
+		// Clean up packet
+		destructSingle(packet);
+		return;
+	}
+
+	// Unqueue processed packets if necessary (done in the playernode (voice)
+
+	// Queue new buffers
+	for (U32 i = 0; i < num; ++i)
+	{
+		SFXInternal::SFXStreamPacket* packet = packets[i];
+		AVAudioFormat* format = mAVBuffer->format;
+
+		// Ensure format is valid and create AVAudioPCMBuffer
+		if (!format) {
+			continue;
+		}
+
+		mAVBuffer->pcmBuffer = [[AVAudioPCMBuffer alloc] initWithPCMFormat:format       frameCapacity:packet->mSizeActual / format.streamDescription->mBytesPerFrame];
+		mAVBuffer->pcmBuffer.frameLength = mAVBuffer->pcmBuffer.frameCapacity;
+
+		// Copy data to AVAudioPCMBuffer
+		memcpy(mAVBuffer->pcmBuffer.audioBufferList->mBuffers[0].mData, packet->data, packet->mSizeActual);
+
+		// Clean up packet
+		destructSingle(packet);
+	}
 }
 
 void SFXAPPLEBuffer::_flush() {
